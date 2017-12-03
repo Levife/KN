78ca549a58664e4c1e4a5f6c482442d54c3a9016
Levife <33778128+Levife@users.noreply.github.com>
2017-12-03 09:55:30 +0800

    first commit

---
+++ venv/python.py
@@ -0,0 +1,17 @@
+#import kNN
+from numpy import *
+
+def main():
+    dataSet, labels = createDataSet()
+
+    testX = array([1.2, 1.0])
+    k = 3
+    outputLabel = kNN.kNNClassify(testX, dataSet, labels, 3)
+    print("Your input is:", testX, "and classified to class: ", outputLabel)
+
+    testX = array([0.1, 0.3])
+    outputLabel = kNN.kNNClassify(testX, dataSet, labels, 3)
+    print("Your input is:", testX, "and classified to class: ", outputLabel)
+
+if __name__=="__main__":
+    main()
